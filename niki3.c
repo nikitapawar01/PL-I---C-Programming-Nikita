7ce93379b97ca56e90b215a77142bce3d11c79d9
nikitapawar01 <nikitapawar359@gmail.com>
2026-09-01 14:55:50 +0530

    Add files via upload

---
+++ niki3.c
@@ -0,0 +1,16 @@
+niki3.c
+/*
+Program (3) -> Write a program to calculate the area of a circle.
+Solution(1) -> Predefined Values
+*/
+#include <stdio.h>
+int main()
+{
+float radius = 1.1;
+float area;
+
+area=(3.14*radius*radius);
+printf("\n Area of Circle = %f ", area);
+
+return 0;
+}
